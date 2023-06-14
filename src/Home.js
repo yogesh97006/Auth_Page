19b621cdffe2c67f62174fa7736bab9faaa23a5c
Yogesh Saini <yogeshsaini9700@gmail.com>
2023-06-14 11:45:41 +0530

    removed all unused things

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,17 @@
-import { Button } from "react-bootstrap"
 import { useUserAuth } from "./UseContextAuth"
 import { useNavigate } from "react-router-dom"
-import {  useState } from "react"
+
 
 function Home(){
     const {User}=useUserAuth()
-    // const [Error,setError]=useState('')
+  
     const {logOut} =useUserAuth()
     const navigate=useNavigate()
 
-    const handleButton=async ()=>{
-        try{
-            await logOut()
-            navigate('/')
-        }
-        catch(error){
-            console.log(error.message)
-        }
-    }
+  
     return(
         <div>
             <h1>Welcome ! {User.email}</h1>
-            {/* {Error} */}
-            {/* <Button onClick={handleButton}>Log Out</Button> */}
         </div>
     )
 }
